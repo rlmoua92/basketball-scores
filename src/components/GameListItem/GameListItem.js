d62255b983c4b40087ad8861c9994184bdf8d969
Ryan Moua <rlmoua92@gmail.com>
2018-11-16 23:14:06 -0800

    Make winning team bold

--- src/components/GameListItem/GameListItem.js
+++ src/components/GameListItem/GameListItem.js
@@ -4,14 +4,16 @@ import Grid from '@material-ui/core/Grid';
 import './GameListItem.scss';
 
 const GameListItem = (props) => {
+  let homeClass = props.homeScore > props.awayScore ? "winner" : "";
+  let awayClass = props.awayScore > props.homeScore ? "winner" : "";
   return (
     <Grid container item direction="column" className="game-list-item">
-      <Grid container item direction="row">
+      <Grid container item direction="row" className={homeClass}>
         <Grid item xs={6} className="game-list-item-team">{props.homeTeam}</Grid>
         <Grid item xs={3} className="game-list-item-score">{props.homeScore}</Grid>
         <Grid item xs={3} className="game-list-item-time">{props.time}</Grid>
       </Grid>
-      <Grid container item direction="row">
+      <Grid container item direction="row" className={awayClass}>
         <Grid item xs={6} className="game-list-item-team">{props.awayTeam}</Grid>
         <Grid item xs={3} className="game-list-item-score">{props.awayScore}</Grid>
         <Grid item xs={3} className="game-list-item-time"></Grid>
